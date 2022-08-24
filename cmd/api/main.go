701c8398a6565b1374e00321af873a5fe92c6012
Fractal Framing <55963511+fractalframing@users.noreply.github.com>
2022-08-23 19:50:47 -0600

    Graceful termination

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,9 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
-	"log"
-	"net/http"
 	"os"
 	"time"
 
@@ -70,18 +67,7 @@ func main() {
 		logger: logger,
 		models: data.NewModels(db),
 	}
-
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		ErrorLog:     log.New(logger, "", 0),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
-
-	logger.PrintInfo("starting server", map[string]string{"env": cfg.env, "addr": srv.Addr})
-	err = srv.ListenAndServe()
+	err = app.serve()
 	logger.PrintFatal(err, nil)
 }
 
